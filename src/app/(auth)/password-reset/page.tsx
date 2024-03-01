47d6b02ceba632d420973b4f76a2626b843dc563
BigBlobby <tom_dempster@hotmail.co.uk>
2024-03-01 21:36:04 +0000

    refactor

--- src/app/(auth)/password-reset/page.tsx
+++ src/app/(auth)/password-reset/page.tsx
@@ -8,26 +8,20 @@ export default function PasswordResetPage(){
     const [state, formAction] = useFormState(resetPasswordAction, null)
 
     return (
-        <div>
-            <section className="bg-gray-50 dark:bg-gray-900">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Reset your password
-                            </h1>
-                            <p>Enter your email address so we can send you a link to reset your password</p>
-                            <form action={formAction} className="space-y-4 md:space-y-6">
-                                <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
-                                </div>
-                                <SubmitWithStatus text="Send link" />
-                            </form>
-                        </div>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Reset your password
+                </h1>
+                <p className="text-sm text-gray-900 dark:text-gray-400">Enter your email address so we can send you a link to reset your password.</p>
+                <form action={formAction} className="space-y-4 md:space-y-6">
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                     </div>
-                </div>
-            </section>
+                    <SubmitWithStatus text="Send link" />
+                </form>
+            </div>
         </div>
     )
 }
